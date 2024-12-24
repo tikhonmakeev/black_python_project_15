ce8e6c7c6f40e58dcbe4eeeed380f8bab4c4f010
tikhonmakeev <tihon.makeev@yandex.ru>
2024-12-24 20:16:58 +0300

    fixed buttons for api(doesnt have 3days)

--- keyboards/interval.py
+++ keyboards/interval.py
@@ -1,10 +1,11 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+
 def interval_keyboard():
     keyboard = InlineKeyboardMarkup(row_width=3)
     keyboard.add(
         InlineKeyboardButton("1 день", callback_data="1"),
-        InlineKeyboardButton("3 дня", callback_data="3"),
-        InlineKeyboardButton("5 дней", callback_data="5")
+        InlineKeyboardButton("5 дней", callback_data="5"),
+        InlineKeyboardButton("10 дней", callback_data="10")
     )
     return keyboard
